13d9a4abb7e5973a34efbd00a2c206977f29f2ff
Raisa Zarin <raisazarinn@gmail.com>
2024-01-31 22:20:12 +0000

    created stg.payments.sql

--- models/staging/stripe/stg.payments.sql
+++ models/staging/stripe/stg.payments.sql
@@ -1,2 +1,16 @@
- select *
-from `dbt-tutorial`.stripe.payment
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount,
+        created,
+        _batched_at as batched_at,
+
+    from `dbt-tutorial`.stripe.payment
+
+)
+
+select * from payments
