32b136a459edc49ab7837c9c64f12e103749fa61
Raisa Zarin <raisazarinn@gmail.com>
2024-02-01 00:37:25 +0000

    changed fct_orders and dim_customers tables.

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,9 +5,9 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount,
-        created,
-        _batched_at as batched_at,
+        amount / 100 as amount,
+        created as created_at,
+        _batched_at as batched_at
 
     from `dbt-tutorial`.stripe.payment
 
