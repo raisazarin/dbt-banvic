f64aabc227a53d8c27cf57580034b704cbfe8796
Raisa Zarin <raisazarinn@gmail.com>
2024-02-04 20:09:03 +0000

    creating dim_clientes.sql

--- analyses/lista_ceps.sql
+++ analyses/lista_ceps.sql
@@ -3,7 +3,13 @@ with clientes as (select * from {{ ref('stg_clientes') }})
 select
 endereco_cliente,
 right(endereco_cliente,2) as estado,
-{{ dbt.datediff(data_nasc_cliente,data_incl_cliente,year) }} as idade
+extract (year from CURRENT_DATE) - EXTRACT(YEAR FROM data_nasc_cliente) as idade,
+CASE 
+    when idade < 25 THEN '< 25 anos'
+    when idade > 25 and < 40 THEN '26 a 40 anos'
+    when idade > 40 and < 60 THEN '41 a 60 anos'
+    else as '> de 60 anos'
+  END as faixa_etaria
 from clientes
 
 
