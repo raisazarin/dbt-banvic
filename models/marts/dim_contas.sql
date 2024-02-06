c2cf956160f570c251c952ca12a9d3054a780ba3
Raisa Zarin <raisazarinn@gmail.com>
2024-02-06 21:18:37 +0000

    a change at dim.customers.sql

--- models/marts/dim_contas.sql
+++ models/marts/dim_contas.sql
@@ -17,10 +17,9 @@ contas as (select * from {{ ref('int_contas') }})
         data_ultimo_lancamento,
         meses_inatividade,
         case when meses_inatividade between 0 and 6 then 'ativa'
-             when meses_inatividade between 7 and 1000 and saldo_total > 200 then 'ativa'
+             when saldo_total > 200 then 'ativa'
              else 'inativa'
-        end as status_conta,
-        1/total_contas as percent_conta
+        end as status_conta
         
     from contas
     
